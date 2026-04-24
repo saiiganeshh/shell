d6919a1e89243eac88b300384f08cc5606b80c63
saiiganeshh <saiganeshsarmaprakki@gmail.com>
2026-04-24 19:20:15 +0530

    six

---
+++ 05-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+START_TIME=$(date +%s)
+echo "script executed at $START_TIME"
+sleep 10
+END_TIME=$(date +%s)
+echo "script executed at $END_TIME"
+
+TOTAL_TIME=$(($START_TIME-$ENDTIME))
+
+echo " sript exeuted in :$TOTAL_TIME 
