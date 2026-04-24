a7cbea6edfb2f7dddcb7e4f45055083c0af0e595
saiiganeshh <saiganeshsarmaprakki@gmail.com>
2026-04-24 13:28:39 +0530

    thhird commit

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=GANESH
+PERSON2=SARADA
+echo "$PERSON1:: hi , $PERSON2 how are you?"
+echo "$PERSON2:: hi $PERSON1, im good how are you , nice to meet you".
+echo "$PERSON1:: so how is your life $PERSON2 whare are u learning ?"
+echo "$PERSON2:: i am learning devops $PERSON1"
