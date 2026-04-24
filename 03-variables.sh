b687de5f01da781c506c5b2df1157b739c917b26
saiiganeshh <saiganeshsarmaprakki@gmail.com>
2026-04-24 17:46:01 +0530

    fourth commit

---
+++ 03-variables.sh
@@ -0,0 +1,6 @@
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: hi , $PERSON2 how are you?"
+echo "$PERSON2:: hi $PERSON1, im good how are you , nice to meet you".
+echo "$PERSON1:: so how is your life $PERSON2 whare are u learning ?"
+echo "$PERSON2:: i am learning devops $PERSON1"
