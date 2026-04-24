6af6f334e2b0bb969ed3f8fd28428190067566b8
saiiganeshh <saiganeshsarmaprakki@gmail.com>
2026-04-24 13:15:33 +0530

    second commit

--- 01-conversation.sh
+++ 01-conversation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "ganesh:: hi , sarada how are you?"
 echo "sarada:: hi ganesh, im good how are you , nice to meet you".
-
+echo "ganesh:: so how is your life sarada whare are u learning ?"
+echo "sarada:: i am learning devops ganesh"
 
 
